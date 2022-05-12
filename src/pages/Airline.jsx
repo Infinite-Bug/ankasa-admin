716a1d730b9711a2ab106d0710b6225a575d533b
hakamfaza <hakamfaza@gmail.com>
2022-05-12 10:17:20 +0700

    update style

--- src/pages/Airline.jsx
+++ src/pages/Airline.jsx
@@ -4,7 +4,8 @@ import CardList from '../components/ListCard'
 export default function Airline () {
   return (
     <div>
-      <CardList/>
+      <CardList title="Garuda Indonesia" src="https://s3-alpha-sig.figma.com/img/d670/ed6a/9d205fa306085ffa6cc1365eef78958f?Expires=1653264000&Signature=aeMyM4YBSavA9gbI7ZCNc4fO07mMu5TvWoUASw38is6LYiEKdzG4dyktJ1VOns8pZOE7Eu0rSkp-1KCeNp-fSLyQo9JWne7Fno3jT9c4PD1lbizXrG~2YSqy0wkufTYxg-fDj8jkdlkN0ipQLROobGbA102f80J6DNrWYE9fblL~~iDeGGgc~n5-qZaiYskQ7awQMAzzlzSLKb8mAwieDKdwjZZzqQobk9o3AXGu1LOC-TIC6mUIhdSTDg7UJSslS7ifEEzmHRGqpmm2vvTXm2wc~tLd-szR21NMe3IebAcLmQ5dM58J~Y7G38AcABfStrcqr8nn5KVskpHWviv5Tg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" pic="Muhamad Hakam Faza" phone="082314970112" />
+      <CardList src="https://s3-alpha-sig.figma.com/img/d2d5/5860/4c04453948ea266977fb0d4595f0fb73?Expires=1653264000&Signature=RgaJwumQbwvYMv03HAu8njySOuMxi5V2PQw72AU9bOU7MVLlkm4Q53YlbKAL64hracx0MMMHwfM3ZreedyBu1wXzX7udCtxUkMTRtQsUK42uCO~diCGdLCyDgM3acFsrii3duCkQrPwlOPo3YSg4SJCL8xmVeoYVGEuZbpHhDMV9ySqBzMTWwyPPWF8QK9o5~tqjgFnubyL7hrc0KOhrJ2EVG-ylgG~FbQYNxM-wC6u5gWOWW4PP-pjdt-5YbJ4qUoe2bDd2Wce8ypR0v5DLiicAFoco8wkuxa3Hh9x~IjAtMYdfypOiDsSpnV4ySLSIUIh79nclDgWoMzhc0DdiDg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" title="Air Asia" pic="Ian Pangestu" phone ="0823234234" />
     </div>
   )
 }
