0b7e009f4735b823cc1fd28a6a85d77269a3939c
hakamfaza <hakamfaza@gmail.com>
2022-05-12 17:03:10 +0700

    integration airline

--- src/pages/Airline.jsx
+++ src/pages/Airline.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import CardList from '../components/ListCard'
+import {getListAirline} from '../redux/actions/airline'
 
-export default function Airline () {
+export default function Airline() {
+  const dispatch = useDispatch()
+  
+  const airline = useSelector((state) => {
+    return state.airline
+  })
+  console.log(airline)
+
+  useEffect(() => {
+    dispatch(getListAirline())
+  }, [dispatch])
+
   return (
     <div>
-      <CardList title="Garuda Indonesia" src="https://s3-alpha-sig.figma.com/img/d670/ed6a/9d205fa306085ffa6cc1365eef78958f?Expires=1653264000&Signature=aeMyM4YBSavA9gbI7ZCNc4fO07mMu5TvWoUASw38is6LYiEKdzG4dyktJ1VOns8pZOE7Eu0rSkp-1KCeNp-fSLyQo9JWne7Fno3jT9c4PD1lbizXrG~2YSqy0wkufTYxg-fDj8jkdlkN0ipQLROobGbA102f80J6DNrWYE9fblL~~iDeGGgc~n5-qZaiYskQ7awQMAzzlzSLKb8mAwieDKdwjZZzqQobk9o3AXGu1LOC-TIC6mUIhdSTDg7UJSslS7ifEEzmHRGqpmm2vvTXm2wc~tLd-szR21NMe3IebAcLmQ5dM58J~Y7G38AcABfStrcqr8nn5KVskpHWviv5Tg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" pic="Muhamad Hakam Faza" phone="082314970112" />
-      <CardList src="https://s3-alpha-sig.figma.com/img/d2d5/5860/4c04453948ea266977fb0d4595f0fb73?Expires=1653264000&Signature=RgaJwumQbwvYMv03HAu8njySOuMxi5V2PQw72AU9bOU7MVLlkm4Q53YlbKAL64hracx0MMMHwfM3ZreedyBu1wXzX7udCtxUkMTRtQsUK42uCO~diCGdLCyDgM3acFsrii3duCkQrPwlOPo3YSg4SJCL8xmVeoYVGEuZbpHhDMV9ySqBzMTWwyPPWF8QK9o5~tqjgFnubyL7hrc0KOhrJ2EVG-ylgG~FbQYNxM-wC6u5gWOWW4PP-pjdt-5YbJ4qUoe2bDd2Wce8ypR0v5DLiicAFoco8wkuxa3Hh9x~IjAtMYdfypOiDsSpnV4ySLSIUIh79nclDgWoMzhc0DdiDg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" title="Air Asia" pic="Ian Pangestu" phone ="0823234234" />
+      {
+        airline.data.map((item, index) => (
+          <div key={index}>
+            <CardList title={item.name} pic={item.pic} phone={item.phone} src={`${process.env.REACT_APP_UPI_URL}/${item.photo}`} />
+          </div>
+        ))
+      }
     </div>
   )
 }
