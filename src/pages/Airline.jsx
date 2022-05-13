e9af60954edec45949b1606d1245824c2fe58873
hakamfaza <hakamfaza@gmail.com>
2022-05-13 18:17:12 +0700

    update integration

--- src/pages/Airline.jsx
+++ src/pages/Airline.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import CardList from '../components/ListCard'
 import {getListAirline, deleteAirline, suspend} from '../redux/actions/airline'
 
 export default function Airline() {
+  const navigate = useNavigate()
   const dispatch = useDispatch()
   
   const airline = useSelector((state) => {
@@ -19,7 +21,6 @@ export default function Airline() {
       .then((res) => {
         console.log(res)
         dispatch(getListAirline())
-        alert('hallo')
       }).catch((err) => {
   console.log(err.message)
 })
@@ -38,7 +39,7 @@ export default function Airline() {
       {
         airline.data.map((item, index) => (
           <div key={index}>
-            <CardList title={item.name} pic={item.pic} phone={item.phone} date={item.created_date} src={`${process.env.REACT_APP_API_URL}/${item.photo}`} isActive={item.is_active ? "Active" : "Non Active"} btnIsActive={() => onSuspend(item.id, item.is_active ? false : true)} btnUpdate={() => alert('Update')} btnDelete={() => onDelete(item.id)} />
+            <CardList title={item.name} pic={item.pic} phone={item.phone} date={item.created_date} src={`${process.env.REACT_APP_API_URL}/${item.photo}`} isActive={item.is_active ? "Active" : "Non Active"} btnIsActive={() => onSuspend(item.id, item.is_active ? false : true)} btnUpdate={() => alert(item.id)} btnDelete={() => onDelete(item.id)} />
           </div>
         ))
       }
