f0e082915e83ba3ee93c99976b0726506f34e234
hakamfaza <hakamfaza@gmail.com>
2022-05-15 11:20:07 +0700

    update airline

--- src/pages/Airline.jsx
+++ src/pages/Airline.jsx
@@ -15,6 +15,8 @@ export default function Airline() {
 
   useEffect(() => {
     dispatch(getListAirline())
+    document.title = `${process.env.REACT_APP_APP_NAME} | Airline`
+    window.scrollTo(0, 0)
   }, [dispatch])
 
   const onDelete = (id) => {
@@ -57,7 +59,7 @@ export default function Airline() {
       {
         airline.data.map((item, index) => (
           <div key={index}>
-            <CardList title={item.name} pic={item.pic} phone={item.phone} date={item.created_date} src={`${process.env.REACT_APP_API_URL}/${item.photo}`} isActive={item.is_active ? "Active" : "Non Active"} btnIsActive={() => onSuspend(item.id, item.is_active ? false : true)} btnUpdate={() => alert(item.id)} btnDelete={() => onDelete(item.id)} />
+            <CardList title={item.name} pic={item.pic} phone={item.phone} date={item.created_date} src={`${process.env.REACT_APP_API_URL}/${item.photo}`} isActive={item.is_active ? "Active" : "Non Active"} btnIsActive={() => onSuspend(item.id, item.is_active ? false : true)} btnUpdate={() => navigate(`${process.env.REACT_APP_API_URL}/airline/${item.id}`)} btnDelete={() => onDelete(item.id)} />
           </div>
         ))
       }
