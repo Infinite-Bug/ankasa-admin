28143dce6f75021471d5c3f7b270d9903aaf3826
hanifudin0597 <hanifudin0597@gmail.com>
2022-05-15 21:55:02 +0700

    update create

--- src/pages/CreateAirline.jsx
+++ src/pages/CreateAirline.jsx
@@ -4,10 +4,9 @@ import '../assets/styles/createairline.css'
 import { createAirline } from '../redux/actions/airline'
 // import { useNavigate } from "react-router-dom";
 import Swal from 'sweetalert2'
+import Navbar from '../components/Navbar'
 
 export default function CreateAirline() {
-    localStorage.setItem("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEyMmFiZTJiLWVmZmItNDdjNS04NjQwLTA4Yjk0NDRmNTBmMiIsImxldmVsIjoxLCJpYXQiOjE2NTI1MzU0OTgsImV4cCI6MTY1MjU1NzA5OH0.7tuFDfOlds1FvcOW7VxYmy8xwrYJDtipHst9yg43C7Y")
-    localStorage.setItem("id", "122abe2b-effb-47c5-8640-08b9444f50f2")
 
     // const dispatch = useDispatch()
     // const navigate = useNavigate()
@@ -47,6 +46,7 @@ export default function CreateAirline() {
 
     return (
         <>
+            <Navbar />
             <div className="container-fluid hanifAirline ml-0 mr-0">
                 <div className="card w-100">
                     <div className="row">
